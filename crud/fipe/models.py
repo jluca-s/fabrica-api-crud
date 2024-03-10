124e23298d00527958b07220ebee3a1aef09ea46
Joao Lucas <joaolucasgc@icloud.com>
2024-03-09 21:06:12 -0300

    Criando a model Vehicle e registrando em fipe.admin

--- crud/fipe/models.py
+++ crud/fipe/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Vehicle(models.Model):
+    price = models.CharField(max_length=50)
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    modelYear = models.CharField(max_length=50)
+    fuel = models.CharField(max_length=50)
+    codeFipe = models.CharField(max_length=50, unique=True, primary_key=True)
+    vehicleType = models.CharField(max_length=50)
+    fuelType = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.model
